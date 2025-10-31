9bb5d5efdfda640ac1614f6cb691a7cab9d53bdb
pratchaya748 <patnamo748@gmail.com>
2025-10-31 22:39:35 +0700

    edit config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://pratchaya748.github.io/portfolio/",
+  site: "https://pratchaya748.github.io",
   base: "/portfolio/",
   vite: {
     plugins: [tailwindcss()],
