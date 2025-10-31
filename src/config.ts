df875fd43b4b44c71909fb7910f16b26378ea335
pratchaya <pratchaya748@gmail.com>
2025-10-31 22:15:05 +0700

    first commit

---
+++ src/config.ts
@@ -0,0 +1,84 @@
+export const siteConfig = {
+  name: "Pratchaya Chalermmeekol",
+  title: "Developer",
+  description: "Portfolio website of Pratchaya Chalermmeekol",
+  accentColor: "#1d4ed8",
+  social: {
+    email: "patnamo748@gmail.com",
+    // linkedin: "https://linkedin.com/in/yourprofile",
+    // twitter: "https://x.com/rfitzio",
+    github: "https://github.com/pratchaya748",
+  },
+  aboutMe:
+    "ผมชื่อปรัชญา เฉลิมมีกล เป็นโปรแกรมเมอร์สาย Fullstack ที่มีประสบการณ์ 2 ปีด้าน Automatic Programming และการพัฒนา Web API รวมถึงการจัดการ Server และฐานข้อมูลเบื้องต้น โดยมุ่งเน้นการออกแบบระบบให้ใช้งานง่าย มีประสิทธิภาพ และสามารถขยายต่อได้ในอนาคต",
+  skills: ["Javascript", "React", "Node.js", "Python","SQL", "Github", "Docker","NginX","Automatic program","Database"],
+  projects: [
+    {
+      name: "เว็บไซต์ Redeem poin",
+      description:
+        "เว็บไซต์สำหรับแลกของรางวัลจากแต้มที่ซื้อสินค้า",
+      link: "",
+      skills: ["React", "Node.js", "NginX","SQL"],
+    },
+    {
+      name: "Chatbot Daily report",
+      description:
+        "โปรแกรมส่งรายงานประจำวันไปที่ line และ google chat",
+      link: "",
+      skills: ["Python","SQL"],
+    },
+    {
+      name: "เว็บไซต์สมัครสมาชิกของบริษัท",
+      description:
+        "เว็บ API สำหรับให้ลูกค้าสมัครสมาชิกกับทางบริษัท",
+      link: "",
+      skills: ["React", "Node.js", "NginX","SQL"],
+    },
+    {
+      name: "API Router",
+      description:
+        "API สำหรับใช้ในองค์กรณ์และให้ทางบริษัทภายนอกเชื่อมต่อ",
+      link: "",
+      skills: ["Node.js", "NginX"],
+    },
+  ],
+  experience: [
+    {
+      company: "CPF internship",
+      title: "Internship Developer",
+      dateRange: "01/02/2023 - 01/05/2023",
+      bullets: [
+        "SAP (ABAP Programming)",
+        "เขียนโปรแกรมสำหรับออกรายงานแบบเฉพาะ",
+        "เขียนโปรแกรมเชื่อม SAP กับระบบอื่น",
+      ],
+    },
+    {
+      company: "MTS Gold Group Company",
+      title: "Developer",
+      dateRange: "16/10/2023 - 16/10/2025",
+      bullets: [
+        "ออกแบบโปรแกรมสำหรับทำงานอัตโนมัติ",
+        "ทำเว็บไซต์สำหรับใช้งานในองค์กรณ์",
+        "ทำ chat bot สำหรับออกรายงานประจำวัน",
+      ],
+    },
+  ],
+  education: [
+    {
+      school: "มหาวิทยาลัยสวนดุสิต (Suan dusit university)",
+      degree: "วิทยาการคอมพิวเตอร์",
+      dateRange: "2020 - 2023",
+      achievements: [
+        "เกียรตินิยมอันดับ 1 GPA 3.82"
+      ],
+    },
+    {
+      school: "โรงเรียนรัตนโกสินทร์สมโภชบางขุนเทียน (Rattanakosinsomphod Bangkhunthian School)",
+      degree: "วิทยาศาสตร์ - คณิตศาสตร์",
+      dateRange: "2015 - 2019",
+      achievements: [
+      ],
+    },
+  ],
+};
